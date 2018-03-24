513c1d6f0e6879c69f921cdcc83efdbd6778c299
jianguohongliang <mhl123mhl123@sina.com>
2018-03-24 11:13:42 +0800

    second

--- PaasCloudConfig/PaasCloudClient/src/main/java/org/xinmin/App.java
+++ PaasCloudConfig/PaasCloudClient/src/main/java/org/xinmin/App.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class App 
-
 {
     public static void main( String[] args )
     {
